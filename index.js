2d8b37a27ac8d89ac563d37b69849a71636795e5
Syncxv <aruldevarackal1@gmail.com>
2021-11-13 03:01:37 +0530

    Ayyyy no nitro gang for life

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Plugin } = require("powercord/entities");
 const fs = require("fs");
 const path = require('path')
 const commands = require("./commands");
+const {messages} = require('powercord/webpack')
 console.log(commands)
 const saveimages = (name, link) => {
   fs.writeFileSync(path.join(__dirname, 'commands', `${name}.js`), 
@@ -17,7 +18,17 @@ const saveimages = (name, link) => {
 };  `)
 }
 module.exports = class ImgCmd extends Plugin {
+  patchMessages() {
+    messages.sendMessage = (sendMessage => async(id, message, ...params) => {
+      console.log(message)
+      if (message.content.startsWith('<:') || message.content.startsWith('<a:')) {
+          message.content = `${message.validNonShortcutEmojis[0].url.replace('?size=40', '?size=64')}`
+      }
+      return sendMessage(id, message, ...params).catch(() => void 0);
+    })(this.oldSendMessages = messages.sendMessage)
+  }
   async startPlugin() {
+    this.patchMessages()
     powercord.api.commands.registerCommand({
       command: "upload",
       aliases: ["move"],
@@ -88,5 +99,6 @@ module.exports = class ImgCmd extends Plugin {
     powercord.api.commands.unregisterCommand('upload');
     powercord.api.commands.unregisterCommand('memes');
     powercord.api.commands.unregisterCommand('spotify2');
+    messages.sendMessage = this.oldSendMessages
   }
 };
